2796e29613bdf961ad9860ba4a468085c6787174
Ujjwal kumar <uk7320942276@gmail.com>
2025-09-09 13:51:29 +0530

    Update script.js
    Signed-off-by: Ujjwal kumar <uk7320942276@gmail.com>

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ================= CONFIG =================
-const API_KEY = "AIzaSyBx_1zTjVkpYrPHgU8spzrtiVVk4IiTv_0"; // replace with your Gemini API key
+const API_KEY = "AIzaSyBKA93ZNtPv1eh-F9kpMwkPGdFT571eoc0"; // replace with your Gemini API key
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`;
 
 // ================= DOM ELEMENTS =================
@@ -292,3 +292,4 @@ overlay.addEventListener("click", () => {
   document.querySelector(".sidebar").classList.remove("active");
   overlay.classList.remove("active");
 });
+
